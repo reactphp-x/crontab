6ae5d774c49ac94460b3b84731dcdbc4a8ebb42d
wpjscc <wpjscc@gmail.com>
2024-06-22 21:17:49 +0800

    remove process

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ composer require reactphp-framework/crontab -vvv
 
 ## Usage
 
-parent process
 
 ```php
 
@@ -18,33 +17,8 @@ require __DIR__ . '/vendor/autoload.php';
 use Reactphp\Framework\Crontab\Crontab;
 
 new Crontab('*/2 * * * * *', function() {
-    echo date('Y-m-d H:i:s')."-parent-process\n";
+    echo date('Y-m-d H:i:s')."\n";
 });
-```
-
-child process
-
-```php
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-use Reactphp\Framework\Process\ProcessManager;
-use Reactphp\Framework\Crontab\Crontab;
-
-ProcessManager::instance()->initProcessNumber(1);
-
-$stream = ProcessManager::instance()->callback(function ($stream) {
-    new Crontab('*/1 * * * * *', function() use ($stream) {
-        $stream->write(date('Y-m-d H:i:s')."-child-process\n");
-    });
-    return $stream;
-});
-
-$stream->on('data', function ($buffer) {
-    echo $buffer;
-});
-
 ```
 
 ## License
--- examples/crontab.php
+++ examples/crontab.php
@@ -2,33 +2,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Reactphp\Framework\Process\ProcessManager;
 use Reactphp\Framework\Crontab\Crontab;
 
 new Crontab('*/2 * * * * *', function() {
-    echo date('Y-m-d H:i:s')."-parent-process\n";
-});
-
-ProcessManager::instance()->initProcessNumber(1);
-
-$stream1 = ProcessManager::instance()->callback(function ($stream) {
-    new Crontab('*/1 * * * * *', function() use ($stream) {
-        $stream->write(date('Y-m-d H:i:s')."-child-process-1\n");
-    });
-    return $stream;
-});
-
-$stream1->on('data', function ($buffer) {
-    echo $buffer;
-});
-
-$stream2 = ProcessManager::instance()->callback(function ($stream) {
-    new Crontab('*/1 * * * * *', function() use ($stream) {
-        $stream->write(date('Y-m-d H:i:s')."-child-process-2\n");
-    });
-    return $stream;
-});
-
-$stream2->on('data', function ($buffer) {
-    echo $buffer;
+    echo date('Y-m-d H:i:s')."\n";
 });
